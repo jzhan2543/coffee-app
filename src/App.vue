70614080e38662bcfbb262a4d001d878ce7332f9
Jeff Zhan <jeffzhan2008@gmail.com>
2019-08-04 10:31:27 -0400

    plugin

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app> 
+      <p> {{ message }} </p> 
+      <v-tabs fixed-tabs class="elevation-1" id="tabs" > 
+        <v-tab class="tab-names"> Brewing Ratio </v-tab>
+        <v-tab class="tab-names"> Espresso Drinks </v-tab>
+        <v-tab-item> 
+          <v-card flat> 
+            <v-card-text> 
+              <BrewingMethods/>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item> 
+          <v-card flat> 
+            <v-card-text> 
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BrewingMethods from "./components/BrewingMethods"; 
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BrewingMethods
+  },
+  data: function() { 
+    return {
+      message: "JavaS Koffee"
+    }
   }
 }
 </script>
 
-<style>
-#app {
+<style scoped>
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+p { 
+  font-size: 2em; 
+  text-align: center; 
+  margin-top: 20px; 
+  margin-bottom: 20px; 
+}
+div { 
+  font-family: "Avenir", Arial, Helvetica, sans-serif; 
+}
+
+.tabs {
+  margin-top: 30px; 
+}
+
+.tab-names { 
 }
 </style>
---
+++ src/components/BrewingMethods.vue
@@ -0,0 +1,157 @@
+<template> 
+    <div> 
+    <link rel="stylesheet" href="/path/to/material-icons/iconfont/material-icons.css">
+
+    <v-flex xs6 align-self-end> 
+      <v-text-field v-model="search" class="mx-3" label="Search" flat></v-text-field>
+    </v-flex>
+    <v-data-table
+    :headers="headers" :items="desserts" :items-per-page="5" class="elevation-1" :search="search">
+    </v-data-table>
+    </div> 
+</template> 
+
+<script>
+  export default {
+    mounted() { 
+      this.test()
+    },
+    methods: {
+      test() { 
+        // var brewing = 'https://blackbearcoffee.com/resources/83';
+        // fetch(brewing).then(function(res) {
+        //   console.log(res.text());
+        // }).then(function(html) {
+        //   console.log(`html = ${html}`);
+        // });
+        // tableToMeasurements = function tableToMeasurements (table) {
+        // var cupRows = table.find('tr').slice(3,-1),
+        //     coffeeNames = ['ounces', 'grams', 'teaspoons', 'tablespoons', 'cups'],
+        //     waterNames = ['fluidounces', 'cups', 'pints', 'quarts', 'halfgallons', 'milliliters', 'liters'],
+        //     cups = {};
+
+        // cupRows.each(function (i, elem) {
+        //   var cells = cheerio(elem).find('td'),
+        //       coffee = cells.slice(1, 6),
+        //       water = cells.slice(6, cells.length);
+
+        //   cups[i+1] = { 'coffee': {}, 'water': {} };
+
+        //   coffee.each(function (j, elem) {
+        //     cups[i+1].coffee[coffeeNames[j]] = cheerio(elem).text();
+        //   });
+        //   water.each(function (j, elem) {
+        //     cups[i+1].water[waterNames[j]] = cheerio(elem).text();
+        //   });
+        // });
+
+        // return cups;
+        // }
+      }
+    },
+    data () {
+      return {
+        search: '', 
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style> 
